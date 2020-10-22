6c96eef73928772c1db4f87fa32feead58389093
willian <wiliam-m-@hotmail.com>
2020-10-22 01:39:20 -0300

    implementacao das classes

---
+++ modulos/__init__.py
@@ -0,0 +1,9 @@
+import modulos.alfabeto
+import modulos.extrator_posicao
+import modulos.extrator_altura
+import modulos.extrator_proximidade
+
+alfabeto = alfabeto.Alfabeto()
+extrator_posicao = extrator_posicao.Extrator_Posicao()
+extrator_altura = extrator_altura.Extrator_Altura()
+extrator_proximidade = extrator_proximidade.Extrator_Proximidade()
